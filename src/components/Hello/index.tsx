067eb73034f9e74580f56e6fac6702285cdb3ffc
Samuel Alves <samuel.estudo778@gmail.com>
2022-07-12 11:16:51 -0300

    first commit

--- src/components/Hello/index.tsx
+++ src/components/Hello/index.tsx
@@ -1,45 +1,9 @@
-import logo from '~/logo.svg';
+import styles from './styles.module.scss';
 
 const Hello = () => {
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        textAlign: 'center',
-        paddingTop: '50px',
-      }}
-    >
-      <img src={logo} alt='Logo' style={{ height: '150px' }} />
-      <ul
-        style={{
-          padding: 0,
-          listStyle: 'none',
-          fontSize: '1.6rem',
-          display: 'flex',
-          flexDirection: 'column',
-          gap: '14px',
-        }}
-      >
-        <li>🚀 Vite</li>
-        <li>🔥 React</li>
-        <li>📖 TypeScript</li>
-        <li>🔨 Eslint</li>
-        <li>💅 Prettier</li>
-      </ul>
-      <p>Don&apos;t forgot to install Eslint and Prettier in your VSCode</p>
-      <div>
-        <a
-          style={{ color: '#F24C4C' }}
-          target='_blank'
-          href='https://github.com/igdev116/vite-react-ts-eslint-prettier'
-          rel='noreferrer'
-        >
-          Github
-        </a>
-      </div>
+    <div className={styles.hello}>
+      <h1>Hello Knex Team!</h1>
     </div>
   );
 };
